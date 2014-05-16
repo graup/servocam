eeb4af7fec5260b03689f59bc80041b50cd6859d
graup <paul@pgrau.de>
2014-05-16 18:42:34 +0900

    Refactor code

--- controller/servo.py
+++ controller/servo.py
@@ -2,18 +2,21 @@
 
 from __future__ import division
 
-import math
-
 class Servo:
 	position = 0
 
 	def angle_to_time(self, angle):
-		return 20000 * (angle/360)
+		""" Returns pulse width in us """
+		time = 20000 * (angle/360)
+		time = int(time/10) * 10 # Precision of PWM is only 10us
+		return time
 
 	def update_position(self):
+		# only import RPIO here because it might not be
+		# installable on the platform the API is tested on
 		from RPIO import PWM
+
 		servo = PWM.Servo()
-		time = int(self.angle_to_time(self.position))
-		time = math.floor(time / 10) * 10
-		print "Setting PWM to %d" % time 
+		time = self.angle_to_time(self.position)
+		print "Setting PWM to %d..." % time 
 		servo.set_servo(18, time)
